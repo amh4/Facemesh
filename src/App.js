282defe9565fe3764fad37e9265e09009e2e2e3e
Adam Hoar <42139484+amh4@users.noreply.github.com>
2023-01-02 14:37:10 +0000

    First push. Project setup. Dependencies installed and connected to webcam

--- src/App.js
+++ src/App.js
@@ -1,22 +1,101 @@
+// Install dependencies Done
+// Import dependencies Done
+// Setup webcam and canvas Done
+// Define references Done
+// Load facemesh Done
+// Detect function
+// Drawing utilities
+// Load triangulation
+// Setup triangle path
+// Setup point drawing
+// Add drawmesh to detect function
+
 import logo from './logo.svg';
 import './App.css';
+import React, {useRef} from 'react';
+import './App.css';
+import * as tf from "@tensorflow/tfjs";
+import * as facemesh from "@tensorflow-models/facemesh";
+import Webcam from "react-webcam";
+
 
 function App() {
+  // Setup references to webcam and canvas
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+// Load facemesh from tf
+const runFacemesh = async () => {
+  const net = await facemesh.load({
+    inputResolution:{width:640, height:480, scale:0.8}
+  });
+  // Sets interval to run detect every 100ms
+  setInterval(()=>{
+    detect(net)
+  }, 100)
+};
+
+// Detect function
+// Checks webcam is up and running before detecting
+const detect = async (net) => {
+  if (
+    typeof webcamRef.current !== "undefined" &&
+    webcamRef.current !== null &&
+    webcamRef.current.video.readyState === 4
+  ){
+    // Get video proporties
+    const video = webcamRef.current.video;
+    const videoWidth = webcamRef.current.videoWidth;
+    const videoHeight = webcamRef.current.videoHeight;
+
+    // Set video Properties
+    webcamRef.current.video.width = videoWidth;
+    webcamRef.current.video.height = videoHeight;
+
+    // Set canvas properties
+    canvasRef.current.width = videoWidth;
+    canvasRef.current.height = videoHeight;
+
+    // Makde detections
+    const face = await net.estimateFaces(video);
+    console.log(face);
+
+  }
+};
+
+runFacemesh();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position:"absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
+        <canvas
+          ref={canvasRef}
+          style={{
+            position:"absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+            width: 640,
+            height: 480,
+          }}
+        />
       </header>
     </div>
   );
